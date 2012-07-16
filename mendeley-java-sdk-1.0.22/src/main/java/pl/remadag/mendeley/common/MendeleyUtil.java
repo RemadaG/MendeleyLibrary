bb20b0cfe83d5aa179bc9a8c66315cd1de46681f
RemadaG <remadag@gmail.com>
2012-07-16 19:54:17 +0200

    mendeley 1.0.22

---
+++ mendeley-java-sdk-1.0.22/src/main/java/pl/remadag/mendeley/common/MendeleyUtil.java
@@ -0,0 +1,27 @@
+package pl.remadag.mendeley.common;
+
+/**
+ * Mendeley util class with static variables.
+ */
+public class MendeleyUtil {
+
+    /**
+     * The Constant CONSUMER_KEY.
+     */
+    public static final String CONSUMER_KEY = "3791d0fcee5de0ddd6bc6e8571e5a86104e6a60c7";
+
+    /**
+     * The Constant CONSUMER_SECRET.
+     */
+    public static final String CONSUMER_SECRET = "1e3d947b421daf533334867c528facc7";
+
+    /**
+     * The Constant ACCESS_TOKEN.
+     */
+    public static final String ACCESS_TOKEN = "9a191ffabae1c1a666693552d3df231704e725a87";
+
+    /**
+     * The Constant TOKEN_SECRET.
+     */
+    public static final String TOKEN_SECRET = "450ac619e360c8882b0ff0e733b6ffad";
+}
