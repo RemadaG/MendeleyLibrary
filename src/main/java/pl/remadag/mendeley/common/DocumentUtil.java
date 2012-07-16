a7e46b2998dcc47b8a47fc4455f456e6438cd239
RemadaG <remadag@gmail.com>
2012-07-16 20:21:41 +0200

    my hola mundo changes

---
+++ src/main/java/pl/remadag/mendeley/common/DocumentUtil.java
@@ -0,0 +1,11 @@
+package pl.remadag.mendeley.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Marcin
+ * Date: 17.09.11
+ * Time: 20:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class DocumentUtil {
+}
