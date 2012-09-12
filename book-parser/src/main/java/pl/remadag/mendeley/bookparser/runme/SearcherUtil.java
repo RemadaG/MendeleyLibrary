5d11a46d01cd8a4c6988156aa8d00b7f7cfc74bd
RemadaG <remadag@gmail.com>
2012-09-12 11:04:11 +0200

    removed class desc

--- book-parser/src/main/java/pl/remadag/mendeley/bookparser/runme/SearcherUtil.java
+++ book-parser/src/main/java/pl/remadag/mendeley/bookparser/runme/SearcherUtil.java
@@ -1,22 +1,3 @@
-/*
- * Created on 06.09.12
- *
- * Copyright (c) 2002-2010 ComArch S.A.
- *
- * Ten plik zrodlowy jest wlasnoscia firmy ComArch S.A. Uzytkownikiem
- * tego pliku moze byc jedynie osoba upowazniona przez ComArch S.A. z wylaczeniem
- * dostepu osob trzecich.
- *
- * Osoba, ktora znalazla sie w posiadaniu niniejszego pliku nie posiadajac
- * legitymacji prawnej do otrzymania takiego materialu, zobowiazana jest do
- * niezwlocznego zwrocenia niniejszego pliku na adres firmy:
- *
- *  ComArch S.A., al. Jana Pawla II 39a, 31-864 Krakow
- *
- * Rozpowszechnianie, kopiowanie, rozprowadzanie lub inne dzialania o podobnym
- * charakterze jest prawnie zabronione pod rygorem sankcji przewidzianych
- * w szczegolowych regulacjach prawnych.
- */
 package pl.remadag.mendeley.bookparser.runme;
 
 import com.mendeley.oapi.schema.Author;
